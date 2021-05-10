12086d7a05bde0983930444f09a2eec1e15c1e0f
nick-yushenko <59775649+nick-yushenko@users.noreply.github.com>
2021-05-10 18:38:40 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ const heroSlider = new Swiper('#heroSlider', {
     bulletClass: 'pagination-item',
     bulletActiveClass: 'current',
     renderBullet: function (index, className) {
-      let item = templates.querySelector('#js-pagination')
+      let item = templates.querySelector('#js-heroPagination')
 
       return item.innerHTML
     }
@@ -47,7 +47,7 @@ const heroSlider = new Swiper('#heroSlider', {
     // when window width is >= 768px
     760: {
       allowTouchMove: false,
-     
+
     },
   }
 
@@ -61,70 +61,68 @@ heroSlider.on('activeIndexChange', function () {
 });
 
 
-// Слайдер в модальном окне для информации о личном кабинете родителя 
-const modalProfileSlider = new Swiper('#modalProfileSlider', {
-  speed: 100,
-  direction: 'horizontal',
-  slidesPerView: 1,
-  autoHeight: true,
-  navigation: {
-    nextEl: '.modal-profile__arrow.next',
-    prevEl: '.modal-profile__arrow.prev',
-  },
-  spaceBetween: 10,
-  effect: 'fade',
-  init: false,
 
-});
 
 // Слайдер личный кабинет родителя на главной странице 
-let profileSlideOffset = (window.innerWidth - document.querySelector('#wrap').clientWidth) / 2
-const profileSlider = new Swiper('#profileSlider', {
-  speed: 400,
-  direction: 'horizontal',
-  slidesPerView: "auto",
-  slidesOffsetBefore: profileSlideOffset,
-  slidesOffsetAfter: profileSlideOffset,
-  spaceBetween: 10,
+if (document.querySelector('#profileSlider') && document.querySelector('#modalProfileSlider')) {
+  let profileSlideOffset = (window.innerWidth - document.querySelector('#wrap').clientWidth) / 2
+  const profileSlider = new Swiper('#profileSlider', {
+    speed: 400,
+    direction: 'horizontal',
+    slidesPerView: "auto",
+    slidesOffsetBefore: profileSlideOffset,
+    slidesOffsetAfter: profileSlideOffset,
+    spaceBetween: 10,
 
 
-  breakpoints: {
-    // when window width is >= 768px
-    768: {
-      spaceBetween: 30,
+    breakpoints: {
+      // when window width is >= 768px
+      768: {
+        spaceBetween: 30,
 
-      navigation: {
+        navigation: {
 
-        nextEl: '.profile-slider__arrow.next',
-        prevEl: '.profile-slider__arrow.prev',
+          nextEl: '.profile-slider__arrow.next',
+          prevEl: '.profile-slider__arrow.prev',
+        },
       },
-    },
-    // when window width is >= 425
-    425: {
-      spaceBetween: 20,
+      // when window width is >= 425
+      425: {
+        spaceBetween: 20,
+      }
     }
-  }
-});
+  });
 
 
-profileSlider.slides.forEach(function (slide) {
-  slide.addEventListener('click', function (e) {
-    let modalProfile = document.querySelector('.modal-profile')
-    if (modalProfile) {
-      modalProfile.classList.add('active')
-      document.querySelector('body').style.overflow = 'hidden'
-      modalProfileSlider.init()
-      modalProfileSlider.slideTo(profileSlider.clickedIndex)
+  profileSlider.slides.forEach(function (slide) {
+    slide.addEventListener('click', function (e) {
+      let modalProfile = document.querySelector('.modal-profile')
+      if (modalProfile) {
+        modalProfile.classList.add('active')
+        document.querySelector('body').style.overflow = 'hidden'
+        modalProfileSlider.init()
+        modalProfileSlider.slideTo(profileSlider.clickedIndex)
 
-    }
+      }
+    })
   })
-})
 
+  // Слайдер в модальном окне для информации о личном кабинете родителя 
+  const modalProfileSlider = new Swiper('#modalProfileSlider', {
+    speed: 100,
+    direction: 'horizontal',
+    slidesPerView: 1,
+    autoHeight: true,
+    navigation: {
+      nextEl: '.modal-profile__arrow.next',
+      prevEl: '.modal-profile__arrow.prev',
+    },
+    spaceBetween: 10,
+    effect: 'fade',
+    init: false,
 
-
-
-
-
+  });
+}
 // Обрезка текста в карточках новостей
 const newsBlocks = document.querySelectorAll('.js-news-block')
 
@@ -164,3 +162,77 @@ if (modalCloseBtns.length > 0) {
     })
   })
 }
+
+
+
+// слайдер на странице О проекте (слайдер сервисов). имеет сложенный слайдер 
+
+const serviceSlider = new Swiper('#serviceSlider', {
+  // Optional parameters
+  speed: 200,
+  autoHeight: true,
+  allowTouchMove: false,
+  spaceBetween: 25,
+  effect: 'fade',
+
+  pagination: {
+    el: '.service-nav',
+    type: 'bullets',
+    clickable: true,
+    bulletClass: 'service-nav__item ',
+    bulletActiveClass: 'current',
+    renderBullet: function (index, className) {
+      let serviceSlides = document.querySelectorAll('.service-slide')
+      let navItem = index + 1
+      if (serviceSlides[index])
+        navItem = serviceSlides[index].querySelector('.service-slide__title').innerHTML
+
+      return '<div class="' + className + '">' + navItem + '</div>';
+    }
+  },
+
+});
+serviceSlider.on('slideChange', function () {
+  // Прокрутить навигацию до текущего элемента (на декстопах)
+  // текущий булет 
+  // console.log($('.service-nav__item.current').offset().top)
+  // let offset = 0;
+  // $('.service-nav__item').each(function( index ) {
+  //   if (index == serviceSlider.realIndex)
+  //   {
+  //     offset = $(this).offset().top
+  //   }
+  // })
+
+  // $('.service-nav').animate({
+  //   scrollTop: $('.service-nav__item.current').offset().top
+  // }, 300)
+
+})
+
+const servicePhotoSlider = new Swiper('#servicePhotos', {
+  // Optional parameters
+  speed: 400,
+  spaceBetween: 15,
+  loop: true,
+  effect: 'fade',
+  // Navigation arrows
+
+  navigation: {
+    nextEl: '.service-photos-arrow.next',
+    prevEl: '.service-photos-arrow.prev',
+  },
+  pagination: {
+    el: '.service-photos-pagination',
+    type: 'bullets',
+    clickable: true,
+    bulletClass: 'service-photos-pagination__item ',
+    bulletActiveClass: 'current',
+    renderBullet: function (index, className) {
+
+
+      return '<div class="' + className + '"></div>';
+    }
+  },
+
+});
